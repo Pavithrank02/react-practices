b570f485e00e9d00d34e482b454a6306e2879ca7
Pavithran2 <pavithrank02@gmail.com>
2023-01-24 21:28:23 +0530

    basic logic done

--- src/components/Formik.js
+++ src/components/Formik.js
@@ -3,24 +3,33 @@ import React from 'react';
  import * as Yup from 'yup';
  
  const SignupForm = () => {
+
+  const initialValues = {
+    firstName: '', 
+    lastName: '', 
+    email: '' 
+  }
+  const validationSchema= () => {Yup.object({
+    firstName: Yup.string()
+      .max(15, 'Must be 15 characters or less')
+      .required('Required'),
+    lastName: Yup.string()
+      .max(20, 'Must be 20 characters or less')
+      .required('Required'),
+    email: Yup.string().email('Invalid email address').required('Required'),
+  })}
+  const handle = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      localStorage.setItem("form", JSON.stringify(values));
+      setSubmitting(false);
+      
+    }, 400);
+  }
    return (
      <Formik
-       initialValues={{ firstName: '', lastName: '', email: '' }}
-       validationSchema={Yup.object({
-         firstName: Yup.string()
-           .max(15, 'Must be 15 characters or less')
-           .required('Required'),
-         lastName: Yup.string()
-           .max(20, 'Must be 20 characters or less')
-           .required('Required'),
-         email: Yup.string().email('Invalid email address').required('Required'),
-       })}
-       onSubmit={(values, { setSubmitting }) => {
-         setTimeout(() => {
-           alert(JSON.stringify(values, null, 2));
-           setSubmitting(false);
-         }, 400);
-       }}
+       initialValues={initialValues}
+       validationSchema= {validationSchema}
+       onSubmit= { handle}
      >
        <Form>
          <label htmlFor="firstName">First Name</label>
