510d45a73a48d0013f8e23d961bbe28b7e0274a7
Pavithran2 <pavithrank02@gmail.com>
2023-01-24 19:31:06 +0530

    formik

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import { useState } from 'react';
+import SignupForm from './components/Formik';
 import './App.css';
 
 function App() {
-  const [count, setcount] = useState(0)
-
-  const increaseCount = () => {
-    setcount( count + 1);
-    console.log(count);
-  }
-
-  const decreaseCount = () =>{
-    setcount(count - 1);
-    console.log(count);
-  }
+  
   return (
     <div className="App">
-      <p>Count value is: {count}</p>
-      <button onClick={increaseCount}>Counter++</button>
-      <button onClick={decreaseCount}>Counter--</button>
+      <SignupForm />
     </div>
   );
 }
